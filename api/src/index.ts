3fb3ff4b019153654b62262e93ba6855d64306a1
Maximilian Seifert <maximilian.cypher@gmail.com>
2022-11-11 15:05:25 +0100

    Use faker to generate random company data

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,34 @@
 import express from "express";
+import { faker } from "@faker-js/faker";
+
+const SPECIALTIES = [
+  "Carpentry",
+  "Insulation",
+  "Ironworking",
+  "Landscaping",
+  "Masonry",
+  "Painting",
+  "Plumbing",
+  "Welding",
+];
 
 const app = express();
 const port = 3000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
+app.get("/companies", (_req, res) => {
+  const companies = Array(100)
+    .fill(undefined)
+    .map((_, index) => ({
+      id: index + 1,
+      name: faker.company.name(),
+      logoUrl: "https://picsum.photos/200/200",
+      specialties: faker.helpers.arrayElements(
+        SPECIALTIES,
+        faker.datatype.number({ min: 1, max: 4 })
+      ),
+    }));
+
+  res.send(companies);
 });
 
 app.listen(port, () => {
